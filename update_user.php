7979cc297ce5fc902aad7cbc57aad1d643d4223c
JordanDestin <destin.jord@gmail.com>
2017-03-12 15:40:33 -0300

    update_user
    page pour la modification de l'utilisateur

---
+++ update_user.php
@@ -0,0 +1,222 @@
+<?php
+
+require 'inc/connect.php';
+
+$errors = [];
+$post = []; // Contiendra les données épurées <3 <3
+
+
+if(isset($_GET['id']) && !empty($_GET['id']) && is_numeric($_GET['id'])){
+    $user_id = (int) $_GET['id'];
+
+if(!empty($_POST)){
+
+foreach($_POST as $key => $value){
+        $post[$key] = trim(strip_tags($value));
+    }
+     //Vérification sur le nom
+    if(empty($post['firstname'])){
+        $errors[] = 'Le champ nom doit être complété';
+    }
+    
+    //Vérification sur le prénom
+    if(empty($post['lastname'])){
+        $errors[] = 'Le champ prénom doit être complété';
+    }
+
+    //Vérification du password
+    if(strlen($post['password']) < 8 || strlen($post['password']) > 20) {
+		$errors[] = "Le champ Password doit avoir au minimum 8 caractères";
+	}
+
+    //Vérification sur le pseudo
+    if(empty($post['nickname'])){
+        $errors[] = 'Le champ pseudo doit être complété';
+    }
+    
+    //Vérification que l'email soit bien rempli
+    if (!filter_var($post['email'], FILTER_VALIDATE_EMAIL)) {
+     $errors[] = "Cette adresse email est considérée comme invalide.";
+    } 
+    
+    //Vérification du role
+	if(empty($post['role'])){
+	 	$errors[] = 'Veuillez choisir le role';
+     }   
+
+    	if(count($errors) === 0){
+
+		$update = $bdd->prepare('UPDATE users SET lastname = :lastname, firstname = :firstname, password = :password, email = :email, nickname = :nickname, role = :role WHERE id = :iduser');
+
+        $update->bindValue(':idUser', $user_id, PDO::PARAM_INT);
+		$update->bindValue(':lastname', $post['lastname']);
+		$update->bindValue(':firstname', $post['firstname']);
+		$update->bindValue(':password', password_hash($post['password'], PASSWORD_DEFAULT));
+		$update->bindValue(':email', $post['email']);
+		$update->bindValue(':nickname', $post['nickname']);
+		$update->bindValue(':role', $post['role']);
+		
+		if($update->execute())
+		{
+			$success = 'Félicitations votre compte a été modifié';
+            
+		}
+		else
+		{
+			var_dump($update->errorInfo());
+		}
+	}
+	else
+	{
+		$textErrors = implode('<br>', $errors);
+	}
+}
+
+    // On sélectionne l'utilisateur pour être sur qu'il existe et remplir le formulaire
+    $select = $bdd->prepare('SELECT * FROM users WHERE id = :idUser');
+    $select->bindValue(':idUser', $user_id, PDO::PARAM_INT);
+
+    if($select->execute()){
+        $my_user = $select->fetch(PDO::FETCH_ASSOC);
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+
+    <!-- Titre de la Page -->
+    <title>Mise à jour</title>
+
+    <!-- Pour Internet Explorer : S'assurer qu'il utilise la dernière version du moteur de rendu -->
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+
+    <!-- Affichage sans zoom pour les mobiles -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Styles CSS -->
+    <link rel="stylesheet" href="#">
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400" rel="stylesheet">
+
+    <!-- FontAwesome -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+    <!-- HTML5 Shiv -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js" integrity="sha256-sqQlcOZwgKkBRRn5WvShSsuopOdq9c3U+StqgPiFhHQ=" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+
+    <main class="container">
+
+
+    <?php
+        // Affichage du message d'erreur
+        if(isset($textErrors)){
+            echo '<p style="color:red">'.$textErrors.'</p>';
+        }
+        // Affichage du success
+        if(isset($success)){
+            echo '<p style="color:green">'.$success.'</p>';
+        }
+    ?>
+
+        <header class="row">
+            <div class="contact col-xs-12">
+                <h1>Modifier contact</h1>
+            </div>
+        </header>
+
+        <section class="row">
+            <!-- Début du Formulaire -->
+            <div class="col-xs-12">
+                <!-- fin du Formulaire -->
+
+                <form method="post" id="contact" class="form-horizontal" role="form" data-toggle="validator">
+                    <fieldset>
+
+                        <!-- Nom du formulaire-->
+                        <legend>Mise à jour</legend>
+                       
+                    </fieldset>
+
+                    <!-- Nom -->
+                    <div class="col-md-4 form-group">
+                        <label for="lastname">Nom</label>
+                    </div>
+                    <div class="col-md-8 form-group">
+                        <input id="lastname" type="text" name="lastname" placeholder="Nom" class="form-control">
+                    </div>
+
+                    <!-- Prénom -->
+                    <div class="col-md-4 form-group">
+                        <label for="firstname">Prénom</label>
+                    </div>
+                    <div class="col-md-8 form-group">                      
+                        <input id="firstname" type="text" name="firstname" placeholder="Prénom" class="form-control">         
+                    </div>
+
+                    <!-- Password -->
+                    <div class="col-md-4 form-group">
+                        <label for="text">Password</label>
+                    </div>
+                    <div class="col-md-8 form-group">
+                        <input id="password" type="password" name="password" placeholder="password" class="form-control">      
+                    </div>
+
+                    <!-- Pseudo -->
+                    <div class="col-md-4 form-group">
+                        <label for="text">pseudo</label>
+                    </div>
+                    <div class="col-md-8 form-group">         
+                        <input id="nickname" type="nickname" name="nickname" placeholder="Pseudo" class="form-control">                
+                    </div>
+
+                    <!-- Email -->
+                    <div class="col-md-4 form-group">
+                        <label for="text">Email</label>
+                    </div>
+                    <div class="col-md-8 form-group">                      
+                        <input id="email"type="email" name="email" placeholder="Email" class="form-control">                  
+                    </div>
+
+                    <!-- Role -->
+                    <div class="col-md-4 form-group">
+                        <label for="text">role</label>
+                    </div>
+                    <div class="col-md-8 form-group">
+                        
+                        <select class="form-control" id="role" name="role">
+                                    <option>-- Sélectionnez --</option>
+                                    <option>Administrateur</option>
+                                    <option>Auteur</option>
+                        </select>
+                
+                    </div>
+
+                    <!-- Submit -->
+                    <div class="col-lg-12 form-group">
+                        <button type="submit" class="btn btn-primary" name="contact" value="Ajouter Contact">Ajouter contact</button>
+                    </div>
+
+                </form>
+                <!-- Fin de mon formulaire -->
+
+            </div>
+
+        </section>
+
+    </main>
+
+</body>
+
+</html>
